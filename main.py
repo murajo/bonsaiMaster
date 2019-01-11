43f5393e10364920b59301be7c68c0cc8a318af7
joujou <t5000carbon@docomo.ne.jp>
2019-01-11 10:45:30 +0900

    詳細検索仮置き

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request
+import dbtool
  
 app = Flask(__name__)
  
@@ -14,11 +14,25 @@ def matchingView():
 
 @app.route('/bonsaibook')
 def bonsaibookView():
-    return render_template('bonsaiBook.html')
+    return render_template('bonsaiBook.html', name=["黒松","真柏"])
+
+def createSql():
+    kind = request.args.get('kind')
+    size = request.args.get('size')
+    sql = "SELECT * FROM bonsai WHERE "
+    return sql
 
 @app.route('/simulation')
 def simulationView():
     return render_template('simulation.html')
+
+@app.route('/postTest', methods=['GET','POST'])
+def hello():
+    if request.method == 'POST':
+        name = request.form['name']
+    else:
+        name = "no name"
+    return render_template('post.html', name = name)
  
 if __name__ == '__main__':
     app.run()
